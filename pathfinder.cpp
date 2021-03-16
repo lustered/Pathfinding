94a4078c387fafd311c4e1e2a26a69670b5aaa3e
lustered <smellslikelavenders@gmail.com>
2021-03-16 03:10:12 -0400

    Basic graph and file reading

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -1,19 +1,59 @@
-#include "map.h"
+#include "pathfinder.h"
+#include "header-files/d_graph.h"
 #include <sstream>
 
+/* template <typename T> pathfinder<T>::pathfinder(std::ifstream &mapfile) { */
+ pathfinder::pathfinder(std::ifstream &mapfile) {
+  mapfile >> this->rows >> this->cols;
 
-map::map(std::ifstream &fin) { std::cin >> this->rows >> this->cols; }
+  this->value.resize(this->rows, this->cols);
 
-void map::mapToGraph(graph &g) {
-  /* Remove some overhead */
-  /* g.maze.resize(g.r, vector<int>(g.c)); */
-}
+  std::string data;
+  while(mapfile >> data){
+
+    unsigned int r = 0;
+
+    for (unsigned int i = 0; i < data.length(); ++i) {
+
+      if(data[i] == 'X')
+        /* std::cout << " island"; */
+        this->value[r][i] = false;
+      else if(data[i] == 'O' || data[i] == 'Z')
+        /* std::cout << " water"; */
+        this->value[r][i] = true;
+      /* else */
+        // std::cout << " Goal";  In this case we can just get it by checking for m-1xn-1 
+
+    } // End of for-loop
+    std::cout << std::endl;
+
+    /* Continue to the next row */
+    r++;
+  } // End of while loop
+} // End of pathfinder constructor
 
 int main() {
-  std::ifstream mapfile("mymap.txt");
+  std::ifstream mapfile("map1.txt");
+  pathfinder pf(mapfile);
 
-  graph g(mapfile);
-  g.display();
+  /* Graph testing */
+  /* 
+  graph<int> g;
+  g.begin();
+  g.insertVertex(1);
+  g.insertVertex(2);
+  g.insertVertex(3);
+  g.insertVertex(4);
+  g.insertEdge(1, 2, 10);
+  g.insertEdge(1, 3, 10);
+  g.insertEdge(1, 4, 10);
+  g.insertEdge(2, 1, 2);
+  g.insertEdge(3, 1, 5);
+
+  std::cout << "in degree of vertex 1: " << g.inDegree(1) << "\n";
+  std::cout << "out degree of vertex 1: " << g.outDegree(1) << "\n";
+  std::cout << "Weight of edge 1->2" << g.getWeight(1, 2) << "\n";
+  */
 
   return 0;
 }
---
+++ pathfinder.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <vector>
+#include "header-files/d_graph.h"
+#include "header-files/d_matrix.h"
+
+using std::queue;
+using std::stack;
+using std::vector;
+
+class pathfinder {
+public:
+  /* Must have */
+  pathfinder(std::ifstream &fin);
+  void mapToGraph(graph<int> &g);
+  bool findPathRecursive(graph<int> &g, stack<int> &moves);
+  bool findPathNonRecursive1(graph<int> &g, stack<int> &moves);
+  bool findPathNonRecursive2(graph<int> &g, queue<int> &moves);
+  bool findShortestPath1(graph<int> &g, stack<int> &bestMoves);
+  bool findShortestPath2(graph<int> &, vector<int> &bestMoves);
+  void printPath(stack<int> &s);
+  void print(int, int, int, int);
+
+  /* void map::printPath(stack<int> &s); */
+  bool isLegal(int i, int j);
+  void setMap(int i, int j, int n);
+  int getMap(int i, int j) const;
+  int getReverseMapI(int n) const;
+  int getReverseMapJ(int n) const;
+
+  int numRows() { return rows; };
+  int numCols() { return cols; };
+
+private:
+  int rows; // number of latitudes/rows in the map
+  int cols; // number of longitudes/columns in the map
+  matrix<bool> value;
+  matrix<int> mapping; // Mapping from latitude and longitude co-ordinates
+                            // (i,j) values to node index values
+  std::vector<int>
+      reverseMapI; // Mapping from node index values to map latitude i value
+  std::vector<int>
+      reverseMapJ; // Mapping from node index values to map longitude j value
+};
